baf8461f1eede2362fe41a1a7e1c569c7def98fe
csmcharles <csmcharles@live.com>
2018-12-08 14:11:01 +1100

    Updates home page

--- index.js
+++ index.js
@@ -13,15 +13,11 @@ const http = require('http');
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8000;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
 // make express look in the `public` directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', (request, response) => {
-    // ejs render automatically looks in the views folder
     response.render('index');
 });
 
@@ -29,10 +25,6 @@ app.listen(port, () => {
     // will echo 'Our app is running on http://localhost:5000 when run locally'
     console.log('Our app is running on http://localhost:' + port);
 });
-
-setInterval(() => {
-    http.get('http://brian-bot.herokuapp.com');
-}, 900000);
 
 //login to discord
 brian.login(TOKEN);
